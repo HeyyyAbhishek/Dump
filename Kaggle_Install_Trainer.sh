80f0d702ae8f168a42b69d3e7df62e4c8954a068
Abhishek <73667631+HeyyyAbhishek@users.noreply.github.com>
2025-11-23 22:21:16 +0530

    Update Kaggle_Install_Trainer.sh

--- Kaggle_Install_Trainer.sh
+++ Kaggle_Install_Trainer.sh
@@ -1,8 +1,4 @@
-mv /teamspace/studios/this_studio/Dump/* /teamspace/studios/this_studio/
-cd /teamspace/studios/this_studio/Dump
-ls -la
-rm -rf ./* ./.??*
-rm /teamspace/studios/this_studio/Dump
+
 cd /teamspace/studios/this_studio
 # install util
 apt update -qq
